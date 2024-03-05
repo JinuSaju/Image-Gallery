3d6507aad50643efa075c8f4e203ba6ed1d8d0ca
JinuSaju <72699649+JinuSaju@users.noreply.github.com>
2024-03-05 20:50:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,28 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Image gallery</title>
 
+    <link rel="stylesheet" href="style.css">
+    
+  </head>
 
+  <body>
+    <h1>Marvel gallery</h1>
 
-const image = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
-const alts = {
-  'pic1.jpg' : 'Yoru',
-  'pic2.jpg' : 'Phoenix',
-  'pic3.jpg' : 'Raze',
-  'pic4.jpg' : 'Omen',
-  'pic5.jpg' : 'Jett'
+    <div class="full-img">
+      <img class="displayed-img" src="images/pic1.jpg" alt="Closeup of a human eye">
+      <div class="overlay"></div>
+      <button class="dark">Darken</button>
+    </div>
 
-
-}
-
-
-
-for (const image of image) {
-  const newImage = document.createElement('img');
-  newImage.setAttribute('src', `image/${image}`);
-  newImage.setAttribute('alt', alts[image]);
-  thumbBar.appendChild(newImage);
-  newImage.addEventListener('click', e => {
-    displayedImage.src = e.target.src;
-    displayedImage.alt = e.target.alt;
-  });
-}
+    <div class="thumb-bar">
 
 
-btn.addEventListener('click', () => {
-  const btnClass = btn.getAttribute('class');
-  if (btnClass === 'dark') {
-    btn.setAttribute('class','light');
-    btn.textContent = 'Lighten';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-  } else {
-    btn.setAttribute('class','dark');
-    btn.textContent = 'Darken';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-  }
-});
+    </div>
+    <script src="main.js"></script>
+  </body>
+</html>
